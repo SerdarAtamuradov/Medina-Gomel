2921176753233c19232d44e74e0da23f7f90f916
Serdar <serdaratamuradov24@gmail.com>
2021-07-21 14:22:59 +0300

    after editing commits in files

--- js/main.js
+++ js/main.js
@@ -281,35 +281,6 @@
     updateShawarma(product, newPrice, newGramm);
   };
 
-  // var changeProductOrderInfo = function (target) {
-  //   var product = myLib.closestItemByClass(target, "product");
-  // var order = document.querySelector(".popup-order");
-
-  // var productTitle = product.querySelector(".product__title").textContent;
-  // var productSize = product.querySelector(
-  //   ".product__size.is-active"
-  // ).textContent;
-  // var productPrice = product.querySelector(
-  //   ".product__price-value"
-  // ).textContent;
-  // var productImgSrc = product
-  //   .querySelector(".product__img")
-  //   .getAttribute("src");
-
-  // order
-  //   .querySelector(".order-info-title")
-  //   .setAttribute("value", productTitle);
-  // order.querySelector(".order-info-size").setAttribute("value", productSize);
-  // order
-  //   .querySelector(".order-info-price")
-  //   .setAttribute("value", productPrice);
-
-  // order.querySelector(".order-product-title").textContent = productTitle;
-  // order.querySelector(".order-product-price").textContent = productPrice;
-  // order.querySelector(".order__size").textContent = productSize;
-  // order.querySelector(".order__img").setAttribute("src", productImgSrc);
-  // };
-
   catalog.addEventListener("click", function (e) {
     var target = e.target;
 
@@ -328,18 +299,5 @@
       e.preventDefault();
       changeProductSizeShawarma(target);
     }
-    // if (target.classList.contains("product__btn")) {
-    //   e.preventDefault();
-    //   changeProductOrderInfo(target);
-    // }
   });
 })();
-/* product end */
-
-/* map start */
-
-/* map end */
-
-/* form start */
-
-/* form end */
